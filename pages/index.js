0fbf5592e8e61e3b0467b88d915a70ac02a2ee7e
Valerii Bihun <valeriibihun.co@gmail.com>
2022-03-07 17:12:01 -0700

    Refactoring tsx into js format

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+
+import Head from 'next/head'
+
+
+export default function Home() {
+  return (
+    // margin x-auto, padding on horizontal access = 10; margin bottom = 8;
+    <div className="container mx-auto px-10 mb-8">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      
+    </div>
+  )
+}
+
