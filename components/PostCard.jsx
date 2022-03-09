c57d0da9062207764339dd5b7dcaac2635973f79
Valerii Bihun <valeriibihun.co@gmail.com>
2022-03-08 20:09:29 -0700

    Post author and date displayed

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -1,13 +1,40 @@
 import React from 'react'
-import moment from 'moment';
-import Link from 'next/link';
+import moment from 'moment'
+import Link from 'next/link'
 
 const PostCard = ({ post }) => {
   return (
-    <div className='bg-white shadow-lg rounded-lg pd-0 lg:p-8 pb-12 mb-8'>
-     <div className='relative overflow-hidden shadow-md pd-80 bd-6'>
-       <img src={post.featuredImage.url} alt={post.title} className="object-top absolute h-80 q-full object-cocver shadow-lg-rounded-t-lg lg:rounded-lg"/>
-       </div>
+    <div className="pd-0 mb-8 rounded-lg bg-white pb-12 shadow-lg lg:p-8">
+      <div className="relative mb-6 overflow-hidden pb-80 shadow-md">
+        <img
+          src={post.featuredImage.url}
+          alt={post.title}
+          className="q-full object-cocver shadow-lg-rounded-t-lg absolute h-80 object-top lg:rounded-lg"
+        />
+      </div>
+      <h1 className="cursor:pointer mb-8 text-center text-3xl font-semibold transition duration-700 hover:text-pink-600">
+        <Link href={`/post/${post.slug}`}>{post.title}</Link>
+      </h1>
+      <div className="mb-8 block w-full items-center justify-center text-center lg:flex">
+        <div className="mb-4 mr-8 flex w-full items-center justify-center lg:mb-0 lg:w-auto">
+          <img
+            alt={post.author.name}
+            height="30px"
+            width="30px"
+            className="rounded-full align-middle"
+            src={post.author.photo.url}
+          ></img>
+          <p className="alight-middle text-grey-700 ml-2 inline text-lg">
+            {post.author.name}
+          </p>
+        </div>
+        <div className='font-medium text-grey-700'>
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 inline mr-2 text-pink-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
+              </svg>
+              <span className="align-middle">{moment(post.createdAt).format('MMM DD, YYYY')}</span>
+        </div>
+      </div>
     </div>
   )
 }
