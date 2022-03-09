ac0c1b044bfeb96cd3d945b961ffef16f0bc0916
Valerii Bihun <valeriibihun.co@gmail.com>
2022-03-08 19:56:14 -0700

    Displaying post image;

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import moment from 'moment';
+import Link from 'next/link';
 
 const PostCard = ({ post }) => {
   return (
-    <div>
-      {post.title}
-      {post.excerpt}
+    <div className='bg-white shadow-lg rounded-lg pd-0 lg:p-8 pb-12 mb-8'>
+     <div className='relative overflow-hidden shadow-md pd-80 bd-6'>
+       <img src={post.featuredImage.url} alt={post.title} className="object-top absolute h-80 q-full object-cocver shadow-lg-rounded-t-lg lg:rounded-lg"/>
+       </div>
     </div>
   )
 }
