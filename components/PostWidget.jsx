2be92c92f7c528dd577445289a3ca75b9a505d5c
Valerii Bihun <valeriibihun.co@gmail.com>
2022-03-08 21:46:07 -0700

    recent posts card implemented

--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -1,28 +1,50 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 
-import moment from 'moment';
-import Link from 'next/link';
+import moment from 'moment'
+import Link from 'next/link'
 
+import { getRecentPosts, getSimilarPosts } from '../services'
 
-import { getRecentPosts } from '../services';
+const PostWidget = ({ categories, slug }) => {
+  const [relatedPosts, setRelatedPosts] = useState([])
 
-
-const PostWidget = ({ categories, slug}) => {
-  const [relatedPosts, setRelatedPosts] = useState([]);
-   
-useEffect(() => {
-  if(slug) {
-    getSimilarPosts(category, slug)
-    .then((result) => setRelatedPosts(relatedPosts, result))
-  } else {
-    getRecentPosts()
-    .then((result) => setRelatedPosts(relatedPosts, result))
-  }
-}, [third])
-
-
+  useEffect(() => {
+    if (slug) {
+      getSimilarPosts(categories, slug).then((result) => {
+        setRelatedPosts(result);
+      });
+    } else {
+      getRecentPosts().then((result) => {
+        setRelatedPosts(result);
+      });
+    }
+  }, [slug]);
+  console.log(relatedPosts)
   return (
-    <div>PostWidget</div>
+    <div className="mb-8 rounded-lg bg-white p-8 shadow-lg">
+      <h3 className="mb-8 border-b pb-4 text-xl font-semibold">
+        {slug ? 'Related posts' : 'Recent Posts'}
+      </h3>
+      {relatedPosts.map((post) => (
+        <div key={post.title} className="mb-4 flex w-full items-center">
+          <div className="w-16 flex-none">
+            <img
+              alt={post.title}
+              height="60px"
+              width="60px"
+              className="rounded-full align-middle"
+              src={post.featuredImage.url}
+            />
+          </div>
+          <div className='flex-grow ml-4'>
+        <p className='text-gray-500 font-xs'>
+           {moment(post.createdAt).format('MMM DD, YYYY')}
+        </p>
+        <Link href={`/post/${post.slug}`} className='text-md' key={post.title}>{post.title}</Link>
+          </div>
+        </div>
+      ))}
+    </div>
   )
 }
 
--- services/index.js
+++ services/index.js
@@ -39,45 +39,47 @@ export const getPosts = async () => {
   return result.postsConnection.edges
 };
 
-export const getRecentPosts =async () => {
-    const query = gql`
-    query GetPostDetails(){
-        posts(
-            orderBy: createdAr_ASC
-            last: 3
-        ) {
-            title
-            featuredImage {
-                url
-            }
-            createdAt
-            slug
-        }
-    }
-    `
-    const result = await request(graphqlAPI, query)
 
-    return result.posts
-  };
+export const getRecentPosts = async () => {
+  const query = gql`
+    query GetPostDetails() {
+      posts(
+        orderBy: createdAt_ASC
+        last: 3
+      ) {
+        title
+        featuredImage {
+          url
+        }
+        createdAt
+        slug
+      }
+    }
+  `;
+  const result = await request(graphqlAPI, query);
+
+  return result.posts;
+};
   
 // showing all of the related posts where slug is not matching the one that is being displayed right now;
 
-export const getSimilarPosts = async () => {
-    const query = gql`
-    query GetPostDetails($slug: String, $categories: [String!]){
-        posts(
-            where: {slug_not: $slug, AND: {categories_some: {slug_in: $categories}}}
-        ) {
-            title
-            featuredImage {
-                url
-            }
-            createdAt
-            slug
+export const getSimilarPosts = async (categories, slug) => {
+  const query = gql`
+    query GetPostDetails($slug: String!, $categories: [String!]) {
+      posts(
+        where: {slug_not: $slug, AND: {categories_some: {slug_in: $categories}}}
+        last: 3
+      ) {
+        title
+        featuredImage {
+          url
         }
+        createdAt
+        slug
+      }
     }
-    `
-    const result = await request(graphqlAPI, query)
+  `;
+  const result = await request(graphqlAPI, query, { slug, categories });
 
-    return result.posts
-}
+  return result.posts;
+};
