f52c1822c39a6b4f4c9bc5318ef08e76efdcc31f
Valerii Bihun <valeriibihun.co@gmail.com>
2022-03-08 21:30:12 -0700

    getPosts\Similar posts calls for graphcms set up

--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -1,6 +1,26 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
-const PostWidget = () => {
+import moment from 'moment';
+import Link from 'next/link';
+
+
+import { getRecentPosts } from '../services';
+
+
+const PostWidget = ({ categories, slug}) => {
+  const [relatedPosts, setRelatedPosts] = useState([]);
+   
+useEffect(() => {
+  if(slug) {
+    getSimilarPosts(category, slug)
+    .then((result) => setRelatedPosts(relatedPosts, result))
+  } else {
+    getRecentPosts()
+    .then((result) => setRelatedPosts(relatedPosts, result))
+  }
+}, [third])
+
+
   return (
     <div>PostWidget</div>
   )
--- services/index.js
+++ services/index.js
@@ -1,41 +1,83 @@
-import {request, gql} from 'graphql-request';
+import { request, gql } from 'graphql-request'
 
-const graphqlAPI  = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT
+const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT
 
 export const getPosts = async () => {
-    const query = gql`
-      query MyQuery {
-        postsConnection {
-          edges {
-            cursor
-            node {
-              author {
-                bio
-                name
-                id
-                photo {
-                  url
-                }
-              }
-              createdAt
-              slug
-              title
-              excerpt
-              featuredImage {
+  const query = gql`
+    query MyQuery {
+      postsConnection {
+        edges {
+          cursor
+          node {
+            author {
+              bio
+              name
+              id
+              photo {
                 url
               }
-              categories {
-                name
-                slug
-              }
+            }
+            createdAt
+            slug
+            title
+            excerpt
+            featuredImage {
+              url
+            }
+            categories {
+              name
+              slug
             }
           }
         }
       }
-    `;
-  
-    const result = await request(graphqlAPI, query);
-  
-    return result.postsConnection.edges;
-  };
+    }
+  `
 
+  const result = await request(graphqlAPI, query)
+
+  return result.postsConnection.edges
+};
+
+export const getRecentPosts =async () => {
+    const query = gql`
+    query GetPostDetails(){
+        posts(
+            orderBy: createdAr_ASC
+            last: 3
+        ) {
+            title
+            featuredImage {
+                url
+            }
+            createdAt
+            slug
+        }
+    }
+    `
+    const result = await request(graphqlAPI, query)
+
+    return result.posts
+  };
+  
+// showing all of the related posts where slug is not matching the one that is being displayed right now;
+
+export const getSimilarPosts = async () => {
+    const query = gql`
+    query GetPostDetails($slug: String, $categories: [String!]){
+        posts(
+            where: {slug_not: $slug, AND: {categories_some: {slug_in: $categories}}}
+        ) {
+            title
+            featuredImage {
+                url
+            }
+            createdAt
+            slug
+        }
+    }
+    `
+    const result = await request(graphqlAPI, query)
+
+    return result.posts
+}
