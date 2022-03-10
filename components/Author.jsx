a51086527ac88ac0afc882c848cd84943a8d1106
Valerii Bihun <valeriibihun.co@gmail.com>
2022-03-09 23:39:56 -0700

    Author photo displayed and styled

--- components/Author.jsx
+++ components/Author.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from 'react';
+import Image from 'next/image';
+// importing Image tag from Next.Js
 
-const Author = ({author}) => {
+const Author = ({ author }) => {
   return (
-    <div>
-      <img src={author.photo.url} alt={author.name} height='100px' width='100px' className='align-middle rounded-full' />
+    <div className="relative mt-20 mb-8 rounded-lg bg-black bg-opacity-20 p-12 text-center">
+      <div className="absolute left-0 right-0 -top-14">
+        {/* Image component but it supports static side generation with Next.Js */}
+        <Image
+          src={author.photo.url}
+          unoptimized
+          alt={author.name}
+          height="100px"
+          width="100px"
+          className="rounded-full align-middle"
+        />
+        
       </div>
+      <h3 className="my-4 text-xl font-bold text-white">{author.name}</h3>
+        <p className="text-lg text-white">{author.bio}</p>
+    </div>
   )
 }
 
-export default Author
+export default Author;
